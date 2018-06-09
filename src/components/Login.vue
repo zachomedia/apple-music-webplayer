dc4ae5456c33e7fa0cc9223c299ea7dee61388f5
Zachary Seguin <zachary@zacharyseguin.ca>
2018-06-09 16:36:10 -0400

    Add link to GitHub project on login screen

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,7 +6,7 @@
     
     <h2 class="h5">Hello!</h2>
     <p>This is a web player for <a href="https://applemusic.com">Apple Music</a>.</p>
-    <p>The web player is currently under development. 
+    <p>The web player is currently under <a href="https://github.com/zachomedia/apple-music-webplayer">development</a>. 
       At this time, you can listen to songs, albums and playlists in your library.
       <br>
       The player current has some issues on small screen sizes.
@@ -17,7 +17,9 @@
       <p class="mb-0">This system is built on an API that is currently in beta. The application may stop working at any time.</p>
     </div>
 
-    <p>If you encounter any issues, have any feedback or feature requests, please email <a href="mailto:contact@zacharyseguin.ca">contact@zacharyseguin.ca</a>.</p>
+    <p>If you encounter any issues, have any feedback or feature requests, 
+      please <a href="https://github.com/zachomedia/apple-music-webplayer/issues">submit an issue on GitHub</a> 
+      or send an email to <a href="mailto:contact@zacharyseguin.ca">contact@zacharyseguin.ca</a>.</p>
 
     <p class="mt-5 text-center">
       <b-button variant="primary" v-on:click.prevent="authorize()">Sign in to Apple Music</b-button>
